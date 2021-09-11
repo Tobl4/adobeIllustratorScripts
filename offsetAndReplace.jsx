5119cc638f7c5c23e246d07f96f6d38d088db71d
Tobias Lunte <tobias.lunte@uol.de>
2021-09-11 03:14:42 +0200

    Updated offsetAndReplace:
    - Now handles grouped and compound paths
    - no longer deletes original if offset was canceled.

--- offsetAndReplace.jsx
+++ offsetAndReplace.jsx
@@ -1,16 +1,17 @@
 /*
   offsetAndReplace.jsx for Adobe Illustrator
-  Version: 1.0
-  Description: Open the offset dialog for currently selected items and delete the orignal paths afterwards. If you cancel the offset dialog, the original paths (but not other objects) will still be deleted and you may want to undo that deletion.
+  Version: 1.1
+  Description: Open the offset dialog for currently selected items and delete the orignal paths afterwards.
   Author: Tobias Lunte, tobias.lunte@uni-oldenburg.de
-  Date: 2021-08-19
+  Contributers: Sergey Osokin
+  Date: 2021-09-10
 
   License:
   Copyright © 2021 Tobias Lunte
   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
   THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+  */
 
 //@target illustrator
 
@@ -20,38 +21,74 @@ function main() {
     return;
   }
   
-  var oldSelect = selection;
-  var nonPathSelect = [];
+  var partitions = partitionSelect(selection);
 
-  for (var i = oldSelect.length - 1; i >= 0; i--) {
-    if (oldSelect[i].typename != "PathItem") {
-      var curNonPath = oldSelect.splice(i, 1)[0];
-      curNonPath.selected = false;
-      nonPathSelect.push(curNonPath);
-    }
-  }
+  var oldPathSelect = partitions.pathSel;
+  var oldNonPathSelect = partitions.nonPathSel;
 
 
-  if (!oldSelect.length) {
-    for (var i = nonPathSelect.length - 1; i >= 0; i--) {
-      nonPathSelect[i].selected = true;
-    }
+  if (!oldPathSelect.length) {
     alert('Error\nPlease select at least one path object.');
     return;
+  }
+  else
+  {
+    for (var i = oldNonPathSelect.length - 1; i >= 0; i--) {
+      oldNonPathSelect[i].selected = false;
+    }
   }
 
   app.executeMenuCommand("OffsetPath v23");
 
-  for (var i = oldSelect.length - 1; i >= 0; i--) {
-    oldSelect[i].remove();
+  if(!isEquals(oldPathSelect, partitionSelect(selection).pathSel)){
+    for (var i = oldPathSelect.length - 1; i >= 0; i--) {
+      oldPathSelect[i].remove();
+    }
   }
 
-  for (var i = nonPathSelect.length - 1; i >= 0; i--) {
-    nonPathSelect[i].selected = true;
+  for (var i = oldNonPathSelect.length - 1; i >= 0; i--) {
+    oldNonPathSelect[i].selected = true;
   }
 
 }
 
+function partitionSelect(sel){
+  var partitions = {pathSel:[], nonPathSel:[]};
+  for (var i =  sel.length - 1; i >= 0; i--) {
+    switch(sel[i].typename){
+      case "GroupItem":
+        var subPar = partitionSelect(sel[i].pageItems);
+        partitions.pathSel = partitions.pathSel.concat(subPar.pathSel);
+        partitions.nonPathSel = partitions.nonPathSel.concat(subPar.nonPathSel);
+        break;
+      case "CompoundPathItem":
+      case "PathItem":
+        partitions.pathSel.push(sel[i]);
+        break;
+      default:
+        partitions.nonPathSel.push(sel[i]);
+        break;
+    }
+  }
+  return partitions;
+}
+
+function isEquals(arr1, arr2) {
+  if (arr1.length != arr2.length) return false;
+
+  for (var i = arr1.length - 1; i >= 0; i--) {
+    var foundMatch = false;
+    for (var j = arr2.length - 1; (!foundMatch) && j >= 0; j--) {
+      if (arr1[i] == arr2[j]) {
+        foundMatch = true;
+      }
+    }
+    if (!foundMatch) {
+      return false;
+    }
+  }
+  return true;
+}
 
 // Run script
 try {
